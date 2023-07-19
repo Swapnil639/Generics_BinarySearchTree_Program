fcf5d20a0711494c2f8898be610cc04087534f66
Swapnil639 <swapnilpatil631997@gmail.com>
2023-07-19 12:01:15 +0530

    Added :Welcome Message

---
+++ src/com/bridgelabz/BinarySearchTree.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class BinarySearchTree {
+    Node root;
+    public static void main(String[] args) {
+        System.out.println("Welcome to Binary search Tree Program");
+    }
+
+
+}

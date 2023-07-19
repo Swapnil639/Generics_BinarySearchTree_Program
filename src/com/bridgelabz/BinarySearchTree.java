99edd1b913495b58eec4a6a6cde43a4d4040cfa6
Swapnil639 <swapnilpatil631997@gmail.com>
2023-07-19 12:05:03 +0530

    Added :Adding elements and print in different ways

--- src/com/bridgelabz/BinarySearchTree.java
+++ src/com/bridgelabz/BinarySearchTree.java
@@ -3,7 +3,39 @@ package com.bridgelabz;
 public class BinarySearchTree {
     Node root;
     public static void main(String[] args) {
-        System.out.println("Welcome to Binary search Tree Program");
+        BinarySearchTree tree=new BinarySearchTree();
+        tree.insert(56);
+        tree.insert(30);
+        tree.insert(70);
+        tree.display();
+        System.out.println();
+    }
+
+    void insert(int data) {
+        root = insertData(root, data);
+    }
+
+    Node insertData(Node root, int data) {
+        if (root == null)
+            root = new Node(data);
+        if (data < root.data) {
+            root.left = insertData(root.left, data);
+        }
+        if (data > root.data) {
+            root.right = insertData(root.right, data);
+        }
+        return root;
+    }
+    void display() {
+        displayNode(root);
+    }
+
+    void displayNode(Node root) {
+        if (root != null) {
+            displayNode(root.left);
+            System.out.println(root.data + ",");
+            displayNode(root.right);
+        }
     }
 
 
